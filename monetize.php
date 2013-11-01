59323a0c7517cdd40148c14f4d1eabceaa16cb6a
Marko Martinović <marko@techytalk.info>
2013-11-01 19:46:02 +0100

    Add link to official site to README

--- monetize.php
+++ monetize.php
@@ -3,14 +3,14 @@
 Plugin Name: Monetize
 Plugin URI: http://www.techytalk.info/
 Description: Monetize units and impressions.
-Version: 1.00
+Version: 1.01
 Author: Marko Martinović
 Author URI: http://www.techytalk.info
 License: GPLv2 or later
 */
 
 class Monetize {
-    const version = '1.00';
+    const version = '1.01';
     const default_db_version = 9;
     const jquery_ui_timepicker_version = '1.4';
     const jquery_swfobject_version = '1.1.1';
